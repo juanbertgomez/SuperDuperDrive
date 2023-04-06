c51bfdc51a1a6b49147dbb0c783c07dee3eda767
Juan Gomez <juan.gomez@optimeeringaqua.com>
2023-04-06 17:13:42 +0200

    file download working

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -1,13 +1,16 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.models.File;
 import com.udacity.jwdnd.course1.cloudstorage.models.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -48,4 +51,27 @@ public class FileController {
         fileService.addFile(fileUpload, user.getUserId());
         return "redirect:/result?success";
     }
+    @GetMapping("/delete")
+    public String deleteFile(@RequestParam("id") int fileId, Authentication authentication) {
+        String loggedInUserName = (String) authentication.getPrincipal();
+        User user = userMapper.getUser(loggedInUserName);
+
+        fileService.deleteFile(fileId);
+        return "redirect:/result?success";
+
+    }
+
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Integer fileId){
+
+        File file = fileService.getFileById(fileId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(file.getContentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+ file.getFileName()+"\"")
+                .body(new ByteArrayResource(file.getFileData()));
+
+
+    }
+
 }
